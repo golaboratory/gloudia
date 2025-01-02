20bb043dd06243709748f6a513d4ca227b4c0b9f
Go Akahane <akahane@golaboratory.jp>
2025-01-02 10:42:49 +0900

    added: godoc

--- core/dolly.go
+++ core/dolly.go
@@ -2,6 +2,9 @@ package core
 
 import "golaboratory/gloudia/core/text"
 
+// Clone は、任意の型 T1 のオブジェクトをシリアライズし、別の任意の型 T2 にデシリアライズしてクローンを作成します。
+// obj: クローンを作成する元のオブジェクト。
+// 戻り値: クローンされたオブジェクトとエラー。
 func Clone[T1 any, T2 any](obj T1) (T2, error) {
 	var result T2
 	json, err := text.SerializeJson(obj)
