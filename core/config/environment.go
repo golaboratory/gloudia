09497a15ccf96db3f14c0fe5d4771031657a4378
Go Akahane <akahane@golaboratory.jp>
2025-02-25 21:49:00 +0900

    added: godoc

--- core/config/environment.go
+++ core/config/environment.go
@@ -7,17 +7,28 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// LoadEnvironments は、カレントディレクトリにある ".env" ファイルから環境変数をロードします。
+// 戻り値:
+//
+//	error - 環境変数のロード中に発生したエラー。正常にロードできた場合はnilを返します。
 func LoadEnvironments() error {
 	return LoadEnvironmentsWithFile(".env")
 }
 
-func LoadEnvironmentsWithFile(path string) error {
-
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return fmt.Errorf("環境ファイルが存在しません: %s", path)
+// LoadEnvironmentsWithFile は、指定されたファイルから環境変数をロードします。
+// 引数:
+//
+//	envFilePath - 読み込む環境変数ファイルのパス。ファイルが存在しない場合はエラーを返します。
+//
+// 戻り値:
+//
+//	error - 環境変数のロード中に発生したエラー。正常にロードできた場合はnilを返します。
+func LoadEnvironmentsWithFile(envFilePath string) error {
+	if _, err := os.Stat(envFilePath); os.IsNotExist(err) {
+		return fmt.Errorf("環境ファイルが存在しません: %s", envFilePath)
 	}
 
-	if err := godotenv.Load(path); err != nil {
+	if err := godotenv.Load(envFilePath); err != nil {
 		return err
 	}
 	return nil
