2a8d2aed18615c323e1527068d20b930f4e272a3
Go Akahane <akahane@golaboratory.jp>
2025-02-26 22:03:38 +0900

    doc: Clone 関数のコメントを更新し、パラメータと戻り値の説明を明確化

--- core/dolly.go
+++ core/dolly.go
@@ -2,9 +2,15 @@ package core
 
 import "github.com/golaboratory/gloudia/core/text"
 
-// Clone は、任意の型 T1 のオブジェクトをシリアライズし、別の任意の型 T2 にデシリアライズしてクローンを作成します。
-// obj: クローンを作成する元のオブジェクト。
-// 戻り値: クローンされたオブジェクトとエラー。
+// Clone は、与えられたオブジェクト obj を JSON シリアライズとデシリアライズを用いてクローンします。
+// オブジェクトの型は、T1 で指定されます。
+// クローンされたオブジェクトの型は、T2 で指定されます。
+// パラメータ:
+//   - obj: クローンする対象のオブジェクト
+//
+// 戻り値:
+//   - T2: クローンされたオブジェクト
+//   - error: クローン処理中に発生したエラー
 func Clone[T1 any, T2 any](obj T1) (T2, error) {
 	var result T2
 	json, err := text.SerializeJson(obj)
